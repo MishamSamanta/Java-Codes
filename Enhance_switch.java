caa93d71c3afc1109991fa6071b4af282c033254
Misham Samanta <misham.samanta@gmail.com>
2026-03-04 22:51:53 +0530

    Enhance code

---
+++ Enhance_switch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Enhance_switch{
+    public static void main(String[] args) {
+        
+        try (Scanner scanner = new Scanner(System.in)){
+
+            System.out.print("Enter a day: ");
+            String day = scanner.nextLine();
+
+            switch (day) {
+                case "Monday" -> System.out.println("This is not a weekend 😒");
+                case "Tuesday" -> System.out.println("This is not a weekend 😒");
+                case "Thursday" -> System.out.println("This is not a weekend 😒");
+                case "Friday" -> System.out.println("This is not a weekend 😒");
+                case "Saturday" -> System.out.println("This is not a weekend 😊");
+                case "Sunday" -> System.out.println("This is not a weekend 😊");
+                default -> System.out.println(day + " doesn't exist");
+            }
+        }
+    }
+}
