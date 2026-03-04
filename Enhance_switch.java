70e4ab0a3add3574e8a0f26bc8c66dfbb225504a
Misham Samanta <misham.samanta@gmail.com>
2026-03-04 22:59:21 +0530

    Update unit converter logic

--- Enhance_switch.java
+++ Enhance_switch.java
@@ -9,12 +9,19 @@ public class Enhance_switch{
             String day = scanner.nextLine();
 
             switch (day) {
-                case "Monday" -> System.out.println("This is not a weekend 😒");
-                case "Tuesday" -> System.out.println("This is not a weekend 😒");
-                case "Thursday" -> System.out.println("This is not a weekend 😒");
-                case "Friday" -> System.out.println("This is not a weekend 😒");
-                case "Saturday" -> System.out.println("This is not a weekend 😊");
-                case "Sunday" -> System.out.println("This is not a weekend 😊");
+                // case "Monday" -> System.out.println("This is not a weekend 😒");
+                // case "Tuesday" -> System.out.println("This is not a weekend 😒");
+                // case "Thursday" -> System.out.println("This is not a weekend 😒");
+                // case "Friday" -> System.out.println("This is not a weekend 😒");
+                // case "Saturday" -> System.out.println("This is not a weekend 😊");
+                // case "Sunday" -> System.out.println("This is not a weekend 😊");
+
+                // OR 
+
+                case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ->
+                        System.out.println("This is not a weekday");
+                case "Saturday", "Sunday" -> 
+                        System.out.println("This is a weekday");
                 default -> System.out.println(day + " doesn't exist");
             }
         }
