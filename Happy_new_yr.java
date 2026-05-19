b6ac87377a54267bc1f6479c0f76f9ad50d7a833
Misham Samanta <misham.samanta@gmail.com>
2026-05-19 16:58:16 +0530

    Time

---
+++ Happy_new_yr.java
@@ -0,0 +1,15 @@
+
+public class Happy_new_yr {
+    public static void main(String[] args) throws InterruptedException{
+        
+        int start = 10;
+
+        for (int i = start; i > 0; i--){
+            System.out.println(i);
+
+            Thread.sleep(1000);
+        }
+
+        System.out.println("Happy New Year!");
+    }
+}
