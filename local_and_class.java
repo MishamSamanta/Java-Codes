7bb0f6d8d326579ada8714cd15179993cd98770f
Misham Samanta <misham.samanta@gmail.com>
2026-05-27 18:19:43 +0530

    Understanding local and class

---
+++ local_and_class.java
@@ -0,0 +1,19 @@
+public class local_and_class {
+
+    static int i = 3;
+    public static void main(String[] args) {
+        int i = 1;
+
+        System.out.println(i);
+
+        anynumber();
+
+    }
+
+    static void anynumber(){
+
+        int i = 2;
+
+        System.out.println(i);
+    }
+}
