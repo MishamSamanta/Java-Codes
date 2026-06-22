dd96245351b98c9770f5f5c256afcdb21c2b7928
Misham Samanta <misham.samanta@gmail.com>
2026-06-22 19:31:44 +0530

    Array Basic

---
+++ class_one.java
@@ -0,0 +1,9 @@
+
+public class class_one {
+    public static void main(String[] args) {
+        
+        String[] fruits = {"apple", "Bananna", "Mango", "Grape"};
+
+        System.out.println(fruits[0]);
+    }
+}
